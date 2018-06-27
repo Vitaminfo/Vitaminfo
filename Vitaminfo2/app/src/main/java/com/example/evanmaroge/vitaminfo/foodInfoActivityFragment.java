1f70419cb720b16d32ac7488c0a9a8ef1545b4f1
Andi Maroge <andimaroge@Andis-MacBook-Pro-2.local>
2018-06-26 18:14:19 -0700

    Added foodActivity and foodInfoActivity. Also created the layout for foodInfoActivity.

---
+++ Vitaminfo2/app/src/main/java/com/example/evanmaroge/vitaminfo/foodInfoActivityFragment.java
@@ -0,0 +1,22 @@
+package com.example.evanmaroge.vitaminfo;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class foodInfoActivityFragment extends Fragment {
+
+    public foodInfoActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_food_info, container, false);
+    }
+}
